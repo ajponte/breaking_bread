851f27369bf75352bb89a753df51d038aa984e61
= <alanjponte@gmail.com>
2015-09-20 00:28:20 -0700

    added crud methods for events

---
+++ app/routes/events.server.routes.js
@@ -0,0 +1,34 @@
+'use strict';
+var mongoose = require('mongoose');
+var Event = mongoose.model('Event');
+var User = mongoose.model('User');
+
+module.exports = function(app) {
+	app.post('api/events/submitEvent', function(req, res) {
+		var data = req.body;
+		var surveyObj = {};
+		var _event = new Event(surveyObj);
+		_event.save(data, function(err, doc) {
+			if (err) {
+				console.log('Error saving event');
+			} else {
+				console.log('Saved Event');
+			}
+		});
+	});
+
+	/**
+	 * Returns all events for the user.
+	 */
+	app.post('/api/events/getEventsForUser', function(req, res) {
+		var data = req.body;
+		Event.find({"user", data.user}, function(err, doc) {
+			if (err) {
+				console.log("error getting event.");
+			} else {
+				res.jsonp(doc);
+			}
+		});
+	});
+};
+
