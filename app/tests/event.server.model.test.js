514881ce7fc526c3cfb99d4a6eac25751896668e
= <alanjponte@gmail.com>
2015-09-20 00:17:40 -0700

    created event model and test

---
+++ app/tests/event.server.model.test.js
@@ -0,0 +1,49 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var should = require('should'),
+	mongoose = require('mongoose'),
+	Event = mongoose.model('Event'),
+	User = mongoose.model('User');
+
+// Globals
+var user1, event1;
+
+describe('Event Model Unit Tests', function() {
+	before(function(done) {
+		user1 = new User({
+			firstName: 'Full',
+			lastName: 'Name',
+			displayName: 'Full Name',
+			email: 'test@test.com',
+			username: 'username',
+			password: 'password',
+			provider: 'local'
+		});
+
+		event1 = new Event({
+			user: user1,
+			name: 'test event1',
+			location: 'test location1',
+			eventType: 'potluck'
+		});
+
+	done();
+
+	});
+
+	describe('Method Save', function () {
+		it('should begin with no events', function(done) {
+			Event.find({}, function(err, events) {
+				events.should.have.length(0);
+				done();
+			});
+		});
+
+		it('should be able to save without problems', function(done) {
+			event1.save(done);
+		});
+	});
+});
