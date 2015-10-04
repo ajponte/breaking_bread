8a8b0f11dd4f29992be88fe79c0fd42a01c2a110
Karlina Beringer <karlinaberinger@Karlinas-MacBook-Air.local>
2015-10-04 10:10:13 -0700

    Started with angular-carousel

--- config/env/all.js
+++ config/env/all.js
@@ -15,6 +15,7 @@ module.exports = {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				'ppublic/lib/angular-carousel/dist/angular-carousel.min.css'
 			],
 			js: [
 				'public/lib/angular/angular.js',
@@ -25,7 +26,8 @@ module.exports = {
 				'public/lib/angular-sanitize/angular-sanitize.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.js',
 				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+				'public/lib/angular-carousel/dist/angular-carousel.min.js'
 			]
 		},
 		css: [
