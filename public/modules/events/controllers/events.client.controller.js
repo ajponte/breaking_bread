5c553dd1fd56b46098ce3409a853db2286d0faaa
= <alanjponte@gmail.com>
2015-10-03 23:31:23 -0700

    Verified and added successfully submitted field

--- public/modules/events/controllers/events.client.controller.js
+++ public/modules/events/controllers/events.client.controller.js
@@ -3,8 +3,6 @@
 angular.module('events').controller('EventsController', ['$scope', '$http',
 	function($scope, $http) {
 		// Controller Logic
-		// 
-		 $scope.testa = true;
 		$scope.createEvent = function() {
 			var participants = $scope.participants.split(",");
 			var eventData = {
@@ -15,18 +13,12 @@ angular.module('events').controller('EventsController', ['$scope', '$http',
 				"dish" : $scope.dish,
 				"maxNumMembers" : $scope.maxNumMembers,
 				"participants" : participants
-
 			};
-
-
-
-
 
 			console.log("eventData: " + JSON.stringify(eventData));
 			$scope.response = '';
 
-			$http.post('/api/events/submitEvent', eventData).then(function(res) 
-			{
+			$http.post('/api/events/submitEvent', eventData).then(function(res) {
 				$scope.response = res;
 				//clear data
 				//console.log(JSON.stringify(res)+"Stuff");
@@ -46,20 +38,8 @@ angular.module('events').controller('EventsController', ['$scope', '$http',
 				$scope.dish='';
 				$scope.maxNumMembers=0;
 				$scope.participants='';
+				$scope.submitted = true;
 
-			//$scope.remove='';
-			 /*
-				$http.post('/someUrl', {msg:'hello word!'}).
-					  then(function(response) {
-					    // this callback will be called asynchronously
-					    // when the response is available
-					  }, function(response) {
-					    // called asynchronously if an error occurs
-					    // or server returns response with an error status.
-					  });
-			 */
-
-			
 		};
 	}
 ]);
